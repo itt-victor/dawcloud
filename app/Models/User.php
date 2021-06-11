ed2f0f07281e3fd4c7088e42122eb36beeb5a5b2
itt-victor <victor.lopez.gomez@outlook.es>
2021-06-11 07:13:00 +0200

    Motor de auth cambiado a Eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Foundation\Auth\User as Auth;
 
-class User extends Model
+class User extends Auth
 {
 	protected $guarded = [];
 
