3852a56835996c10aa5015e3a31ed1a600caa1a1
itt-victor <victor.lopez.gomez@outlook.es>
2021-06-11 08:48:51 +0200

    .

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,6 +9,17 @@ class Project extends Model
 {
 	protected $guarded = [];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    /*protected $fillable = [
+        'project_name',
+        'json_data',
+        'user_id',
+    ];*/
+
 	public function user()
 	{
 		return $this->belongsTo(User::class);
