a072fadbc8b8f109b9c858995b69ce19189e605f
itt-victor <victor.lopez.gomez@outlook.es>
2021-06-19 18:47:59 +0200

    deploy

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ mix.js('resources/js/app_core.js', 'public/js')
     .js('resources/js/home.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps()
-    .disableSuccessNotifications();
+    .disableSuccessNotifications()
+    .options({ processCssUrls: false });
