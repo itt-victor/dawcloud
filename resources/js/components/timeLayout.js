13d572f185d9282762f1d5f72a777aed657f7153
itt-victor <victor.lopez.gomez@outlook.es>
2021-06-19 18:57:26 +0200

    layout permite arrastrar

--- resources/js/components/timeLayout.js
+++ resources/js/components/timeLayout.js
@@ -5,12 +5,29 @@ import { soundcontroller } from '../app_core';
 
 //Interacción con el layout de tiempo
 const timeLayout = document.querySelector('#layout');
+let click = false;
 
-timeLayout.addEventListener('click', function (event) {
-    timeSpace.space = Math.max(event.offsetX, 0);
-    cursor.moveAtClick();
-    if (soundStatuses.isPlaying == true && soundStatuses.hasStopped == false) {
-        soundcontroller.stopSound();
-        setTimeout(soundcontroller.playSound(), 10);
-    }
+timeLayout.addEventListener('mousedown', function (e) {
+    click = true;
+    pointTime(e);
 });
+
+timeLayout.addEventListener('mousemove', pointTime);
+
+window.addEventListener('mouseup', function () {
+    click = false;
+});
+
+function pointTime(event) {
+    if (click) {
+        timeSpace.space = Math.max(event.offsetX, 0);
+        cursor.moveAtClick();
+        if (soundStatuses.isPlaying == true
+            && soundStatuses.hasStopped == false) {
+            soundcontroller.stopSound();
+            setTimeout(soundcontroller.playSound(), 10);
+        }
+    }
+}
+
+
